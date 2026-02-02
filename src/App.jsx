752fa3600fa1e201e95f495e53a30383bc026917
middle-aged-Weiwei <chinweihsu83221@gmail.com>
2026-02-03 02:03:59 +0800

    修正App.jsx作為測試路由用，刪除舊的錯誤命名product_lits.jsx檔

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,9 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 function HomePage() {
   return (
     <>
-      <h1>首頁</h1>
-      <p>這裡是原本 App.jsx 的內容。</p>
+      <h1>首頁(暫時測試區)</h1>
       {/* 👇 只要把這行註解掉，測試功能就會徹底關閉 */}
       {/* <ApiTester />*/}
-      <ProductList />
     </>
   );
 }
@@ -25,13 +23,28 @@ function HomePage() {
 // App 元件現在專職處理路由
 function App() {
   return (
-    <Routes>
-      {/* 前台 */}
-      <Route path="/" element={<HomePage />} />
+    <>
+      {/* --- 不會動的外殼 --- */}
 
-      {/* 後台總入口：交給 AdminIndex 處理所有 /admin/* 路由 */}
-      <Route path="/admin/*" element={<AdminIndex supabase={supabase} />} />
-    </Routes>
+      {/* --- 會根據網址變動的內容區域 --- */}
+      <Routes>
+        {/*請依字母排序各自的路由，path是網頁路徑，例如https://weiwei898.github.io/d-4-submate-react/A/B*/}
+        {/*A=path的內容，只有"/"就是會少一層，d-4submate-react/product*/}
+        {/*A=path的內容，如果是"pages"，d-4submate-react/pages/product*/}
+        {/**B=element裡的檔案路徑，例如product/ */}
+
+        {/* --- 前台公開區域 --- */}
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/products" element={<ProductList />} />
+
+
+        {/* 後台總入口：交給 AdminIndex 處理所有 /admin/* 路由 */}
+        <Route path="/admin/*" element={<AdminIndex supabase={supabase} />} />
+      </Routes>
+      {/* --- 不會動的外殼 --- */}
+
+    </>
   );
 }
 
