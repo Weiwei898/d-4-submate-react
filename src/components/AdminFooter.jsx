51866ece7d29c828c7d92c07138fb459cf3e072c
middle-aged-Weiwei <chinweihsu83221@gmail.com>
2026-03-02 03:55:37 +0800

    後台用header、footer第一版測試

---
+++ src/components/AdminFooter.jsx
@@ -0,0 +1,21 @@
+
+function AdminFooter() {
+    return (
+        <footer className="bg-primary-50">
+            <div className="container">
+                {/* 法律資訊列 */}
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="d-flex py-4 justify-content-center">
+                            <p className="fs-7 text-natural-500">
+                                © 2026 Copyright SubMate. All Rights Reserved
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default AdminFooter;
